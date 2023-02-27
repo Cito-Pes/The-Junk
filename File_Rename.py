4b1e05285a8fb2fae241053a8eef48b7251e1b19
Your Name <you@example.com>
2023-02-27 20:23:39 +0900

    new

--- File_Rename.py
+++ File_Rename.py
@@ -13,6 +13,7 @@ import shutil
 def File_search(dir):
     err_file = []
     filenames = os.listdir(dir)
+    i = 1
     for filename in filenames:
         full_path = os.path.join(dir, filename)
 
@@ -22,13 +23,19 @@ def File_search(dir):
         else:
             # print(f"파일명: {full_path}", filename)
             name, ext = os.path.splitext(filename)
-            # print('name',name, '              ext ::::', ext)
+            print('name',name, '              ext ::::', ext)
+
+            print(name.find("-"))
 
 
+
+        i +=1
+
+    print(i)
     print(err_file)
 
 
 
-File_search('C:/Users/jino0/Cloudike/현대투어존동의자료')
+# File_search('C:/Users/jino0/Cloudike/현대투어존동의자료')
 
-File_search('X:/DOC')
+File_search('z:/temp')
